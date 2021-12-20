ffdc50c05cae21d6597a9d753421ce899546cc95
Bishal Keshri <bishalprasadkeshri@gmail.com>
2021-12-20 12:00:04 -0800

    Count Prime Using Array Reduce

--- Lecture14thDec/Q1-ArrayReduceCountPrimes.js
+++ Lecture14thDec/Q1-ArrayReduceCountPrimes.js
@@ -1,2 +1,22 @@
 // count all primes using reduce
-let arr = [51, 23, 37, 44, 73, 82, 97, 45];
+let arr = [51, 23, 37, 44, 73, 82, 97, 41];
+
+let countingPrimes = arr.reduce(function(pv, cv, ci, oarr) {
+    let flag = true;
+
+    console.log(pv," ", cv);
+    for(let div = 2; div * div <= cv; div++) {
+        if(cv % div == 0) {
+            flag = false;
+            break;
+        }
+    }
+    
+    if(flag == true) {
+        return pv + 1;
+    } else {
+        return pv;
+    }
+},0);
+
+console.log(countingPrimes);
